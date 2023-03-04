17cca05052d74abd8e51a75cbe2880ad9b254f71
Kevin Bueno <kbueno1077@gmail.com>
2023-03-04 12:41:40 -0500

    menu with img

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Image from "next/image";
 
-import { Worker } from "@react-pdf-viewer/core";
-import { Viewer } from "@react-pdf-viewer/core";
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+const menu_photos_prefix = "BLACKOUT_MENU_";
+const menu_photos_cant = [1, 2, 3, 4, 5, 6, 7];
 
 export default function Home() {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
-
   return (
     <>
       <Head>
@@ -18,15 +15,24 @@ export default function Home() {
         <link rel="icon" href="/bcicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div style={{ height: "100vh" }}>
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.3.122/build/pdf.worker.min.js">
-            <Viewer
-              fileUrl="BLACKOUT_MENU.pdf"
-              plugins={[defaultLayoutPluginInstance]}
-            />
-            ;
-          </Worker>
-        </div>
+        {menu_photos_cant.map(photoNumber => {
+          return (
+            <div
+              key={`${photoNumber}`}
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column"
+              }}
+            >
+              <img
+                style={{ objectFit: "contain" }}
+                src={`/menu_photos/${menu_photos_prefix}${photoNumber}.jpg`}
+              />
+            </div>
+          );
+        })}
       </main>
     </>
   );
---
+++ pages/menu_pdf.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+import { Worker } from "@react-pdf-viewer/core";
+import { Viewer } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+
+export default function Menu_PDF() {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+  return (
+    <>
+      <Head>
+        <title>Black Coffee Menú</title>
+        <meta name="description" content="Black Coffee Menú" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/bcicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div style={{ height: "100vh" }}>
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.3.122/build/pdf.worker.min.js">
+            <Viewer
+              fileUrl="BLACKOUT_MENU.pdf"
+              plugins={[defaultLayoutPluginInstance]}
+            />
+            ;
+          </Worker>
+        </div>
+      </main>
+    </>
+  );
+}
