430713cb5c0f7ff8f8f99a0f4e6d8ccc1b740d45
kbueno1077 <kbueno1077@gmail.com>
2023-09-24 13:23:31 -0400

    Change Cloudinary to blackOut.coffeebar@gmail.com

--- helpers/services.js
+++ helpers/services.js
@@ -1,18 +1,17 @@
 const cloudinary = require("cloudinary").v2;
 
 export async function GetImagesService() {
-  cloudinary.config({
-    cloud_name: "dub477vzt",
-    api_key: "763641954252769",
-    api_secret: "Qs2_7_dklt-9I8yskgkoSqELxhA",
-    secure: true,
-  });
+    cloudinary.config({
+        cloud_name: "das9jgkep",
+        api_key: "859843263784411",
+        api_secret: "SjpROD-jgfjX7NRlHcdO9yCZVZU",
+        secure: true,
+    });
 
-  const resources = await cloudinary.search
-    .expression(`folder:blackOut`)
-    .execute();
+    const resources = await cloudinary.search
+        .expression(`folder:blackOut`)
+        .execute();
 
-  const jsonResources = JSON.stringify(resources);
-
-  return jsonResources;
+    const jsonResources = JSON.stringify(resources);
+    return jsonResources;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,55 @@
 import Head from "next/head";
 import Image from "next/image";
 import React from "react";
-import { GetImagesService } from "@/helpers/services";
+import {GetImagesService} from "@/helpers/services";
 
-export default function Home({ assets }) {
-  return (
-    <>
-      <Head>
-        <title>Black Coffee Menú</title>
-        <meta name="description" content="Black Coffee Menú" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/bcicon.ico" />
-      </Head>
-      <div style={{ maxWidth: "1200px", margin: "0 auto 0" }}>
-        {assets.resources.map((asset, number) => {
-          return (
-            <Image
-              key={`${number}`}
-              src={asset.secure_url}
-              width={200}
-              loading="eager"
-              height={160}
-              priority
-              alt={"Menu Image"}
-              sizes="(max-width: 768px) 100vw,
-                (max-width: 1200px) 50vw, 33vw"
-              style={{
-                height: "100%",
-                width: "100%",
-                borderRadius: "20px",
-                padding: "5px",
-              }}
-              //The point is right there!
-              //OR className='w-100 h-100'
-            />
-          );
-        })}
-      </div>
-    </>
-  );
+export default function Home({assets}) {
+    return (
+        <>
+        <Head>
+            <title>Black Coffee Menú</title>
+            <meta name="description" content="Black Coffee Menú"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link rel="icon" href="/bcicon.ico"/>
+        </Head>
+        <div style={{maxWidth: "1200px", margin: "0 auto 0"}}>
+            {assets.resources.map((asset, number) => {
+                return (<Image
+                    key={`${number}`}
+                    src={asset.secure_url}
+                    width={200}
+                    loading="eager"
+                    height={160}
+                    priority
+                    quality={100}
+                    alt={"Menu Image"}
+                    sizes="(max-width: 768px) 100vw,
+                           (max-width: 1200px) 50vw, 33vw"
+                    style={{
+                        height: "100%", width: "100%", borderRadius: "20px", padding: "5px",
+                    }}
+                    //The point is right there!
+                    //OR className='w-100 h-100'
+                />);
+            })}
+        </div>
+    </>);
 }
 
 export async function getServerSideProps() {
-  const jsonResponse = await GetImagesService();
-  const parseResponse = JSON.parse(jsonResponse);
+    const jsonResponse = await GetImagesService();
+    const parseResponse = JSON.parse(jsonResponse);
 
-  parseResponse.resources.sort((a, b) => {
-    if (a.filename < b.filename) {
-      return -1;
-    }
-    if (a.filename > b.filename) {
-      return 1;
-    }
-    return 0;
-  });
+    parseResponse.resources.sort((a, b) => {
+        if (a.filename < b.filename) {
+            return -1;
+        }
 
-  return { props: { assets: parseResponse } };
+        if (a.filename > b.filename) {
+            return 1;
+        }
+        return 0;
+    });
+
+    return {props: {assets: parseResponse}};
 }
